b49afd16b601318cad0088a02c996d26b8e384bf
Vladislav Georgiev <vladislav.g95@outlook.com>
2024-10-10 13:35:16 +0300

    Add tailwind config, app is stable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: 'var(--color-primary-light)',
+          dark: 'var(--color-primary-dark)',
+        },
+        secondary: {
+          light: 'var(--color-secondary-light)',
+          dark: 'var(--color-secondary-dark)',
+        },
+        white: {
+          DEFAULT: 'var(--color-white)',
+        },
+        gray: {
+          disabled: 'var(--color-gray-disabled)',
+          background: 'var(--color-gray-background)',
+          helper: 'var(--color-gray-helper)',
+        },
+        black: {
+          text: 'var(--color-black-text)',
+          ui: 'var(--color-black-ui)',
+          background: 'var(--color-black-background)',
+        },
+      },
+      fontFamily: {
+        heading: ['var(--font-heading)', 'sans-serif'],
+        primary: ['var(--font-primary)', 'serif'],
+      },
+      backgroundImage: {
+        'gradient-primary': 'var(--gradient-primary)',
+      },
+      gradientColorStops: (theme) => ({
+        primary: theme('colors.primary'),
+        secondary: theme('colors.secondary'),
+      }),
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require('@tailwindcss/forms')],
+};
