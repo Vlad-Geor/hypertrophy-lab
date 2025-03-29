82de9defe017c0460e202895a7f4cfdbb5d657c8
Vladislav Georgiev <vladislav.g95@outlook.com>
2025-03-29 23:24:43 +0300

    Add style-dictionary, YET TO MAKE IT WORK FFS

--- apps/hypertrophy-lab/src/app/app.routes.ts
+++ apps/hypertrophy-lab/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Route } from '@angular/router';
-import { DailyWorkoutComponent } from '@ikigaidev/hl/daily-workout';
 import { PlaygroundComponent } from '@ikigaidev/playground';
 import { ShellComponent } from './shell/shell.component';
 
@@ -19,10 +18,6 @@ export const appRoutes: Route[] = [
       },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     ],
-  },
-  {
-    path: 'todays-workout',
-    component: DailyWorkoutComponent,
   },
   {
     path: 'nutrition',
